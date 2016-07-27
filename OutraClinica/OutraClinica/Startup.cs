6c13c95d77d2f3a667a8c98af72731362d6f32b7
joaoberte <joaoberte2006@yahoo.com.br>
2016-07-27 15:32:53 -0300

    [Joao] projeto iniciado

---
+++ OutraClinica/OutraClinica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OutraClinica.Startup))]
+namespace OutraClinica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
