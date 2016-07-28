221cad6dc0cc8a34afa40765374ba1cae7c82f4a
joaoberte <joaoberte2006@yahoo.com.br>
2016-07-28 13:38:08 -0300

    [Joao] Criada Model Medico

---
+++ OutraClinica/OutraClinica/Models/Medico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OutraClinica.Models
+{
+    public class Medico : Funcionario
+    {
+        public string especialidades { get; set; }
+    }
+}
