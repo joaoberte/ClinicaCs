16585d9f39ce63366c16b5a50e9f2db19c91cb6d
joaoberte <joaoberte2006@yahoo.com.br>
2016-07-28 11:35:17 -0300

    [Joao] Criada model Funcionario

---
+++ OutraClinica/OutraClinica/Models/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OutraClinica.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string rg { get; set; }
+
+        public string endereco { get; set; }
+
+        public string login { get; set; }
+
+        public string senha { get; set; }
+    }
+}
